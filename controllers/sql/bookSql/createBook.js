a2f05b5d5ad082b09bc8ceeb8773548a1b8483ee
Bibek Regmi <bibek.regmi@swifttech.com>
2023-07-14 14:50:29 +0545

    last commit

---
+++ controllers/sql/bookSql/createBook.js
@@ -0,0 +1,32 @@
+"use strict";
+const sqlstring = require("sqlstring");
+const helper = require("../../../helper/index.js");
+(() => {
+    module.exports = async (obj) => {
+        try {
+            const { v4: uuidv4 } = require('uuid');
+            const now = new Date().getTime();
+           
+
+            const querystring = sqlstring.format(`INSERT INTO books  (uuid, bookName, bookId, price, issueDate,  createdAt) VALUES (?, ?, ?, ?, ?, ?) `, [
+                uuidv4(),
+                obj.bookName,
+                obj.bookId,
+                obj.price,
+                obj.issueDate,
+                now
+            ]);
+
+
+            const [sqlquery] = await helper.mysqlHelper.query(querystring);
+
+            if (sqlquery.affectedRows > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+})();
--- models/seeders/bookSeeder.js
+++ models/seeders/bookSeeder.js
@@ -1,14 +1,46 @@
+// const { UUIDV4 } = require("sequelize");
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('Users', [
+//       {
+//         uuid: UUIDV4,
+//         bookName: "English",
+//         bookId: "1237654",
+//         price: "560",
+//         issueDate: "2010/1/23",
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       }]);
+//   },
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('Users', null, {});
+//   }
+// };
+
+'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+  up: async (queryInterface, Sequelize) => {
+    const booksData = [];
+    const currentDate = new Date();
+
+    for (let i = 1; i <= 10; i++) {
+      booksData.push({
+        uuid: Sequelize.UUIDV4,
+        bookName: `Book ${i}`,
+        bookId: `BID-${i}`,
+        price: i * 100,
+        issueDate: new Date(currentDate.getTime() + i * 24 * 60 * 60 * 1000),
+        createdAt: currentDate.toISOString(),
+        updatedAt: currentDate.toISOString()
+      });
+    }
+
+    await queryInterface.bulkInsert('books', booksData, {});
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('books', null, {});
   }
 };
